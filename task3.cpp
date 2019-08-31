ce22a7ea9bae5b2bac532d05d31bb16db6918f78
spandanag333 <54741708+spandanag333@users.noreply.github.com>
2019-08-31 21:23:09 +0530

    Add files via upload

---
+++ task3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int year;
+	cout<<"enter a year : ";
+	cin>>year;
+	if(year%4==0)
+		cout<<year<<" is a leap year ";
+		else
+		cout<<year<<" is not a leap year ";
+
+}
